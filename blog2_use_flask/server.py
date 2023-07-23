9db3988be3c7c3201c76d6e0fd298500c378073a
khs <stdonly99@gmail.com>
2023-07-23 14:24:27 +0900

    플라스크 부트스트랩 블로그(이메일보내기기능 추가)

--- blog2_use_flask/server.py
+++ blog2_use_flask/server.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+from email.message import EmailMessage
 
 app = Flask(__name__)
 
 URL = "https://api.npoint.io/c790b4d5cab58020d391"
 response = requests.get(URL)
 data = response.json()
+
+EMAIL = ""  # 보안
+PW = ""  # 보안
 
 
 @app.route("/")
@@ -18,9 +23,29 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
-    return render_template("contact.html")
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        messaget = request.form["message"]
+        sendeamil(name, email, phone, messaget)
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def sendeamil(name, email, phone, messaget):
+    message = EmailMessage()
+    messaget = f"내용 : {messaget}\n이름 : {name}\n발신자 : {email}\n핸드폰 번호 : {phone}"
+    message.set_content(messaget)
+    message["Subject"] = "블로그 컨택 메시지"
+    message["From"] = "블로그"
+    message["to"] = ""  # 보안
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL, password=PW)
+        connection.send_message(message)
 
 
 @app.route("/post/<id>")
